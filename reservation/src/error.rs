293e0b2cc3868609c975db488c9d15aeb9c5e47c
hjkl1 <1161524405@qq.com>
2025-05-16 19:16:45 +0800

    Add repo error

---
+++ reservation/src/error.rs
@@ -0,0 +1,10 @@
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum RepositoryError {
+    #[error("an error occurred while operating the database.")]
+    DatabaseError(#[from] sqlx::Error),
+
+    #[error("invalid start or end time.")]
+    InvalidTimestampRange,
+}
